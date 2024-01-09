951eb15d4b76c74ee48210e4267629fa7c7ed345
Pushkar Borkar <pushkarborkar1809@gmail.com>
2024-01-09 15:55:28 +0530

    remove loader on signup button

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -55,21 +55,11 @@ const SignUpForm = ({ status, message, onValidated, isDark }) => {
 			<input
 				type="submit"
 				disabled={!email}
-				value={status === "sending" ? <Loader /> : "Sign Up"}
+				value="Sign Up"
 				className="bg-primary-500 dark:bg-primary-400 text-light-100 text-sm font-bold rounded py-2 md:px-4 focus:outline-none hover:brightness-110 active:brightness-90 w-full md:w-3/12 disabled:cursor-not-allowed disabled:opacity-70"
 			/>
 		</form>
 	);
 };
-
-const Loader = () => (
-	<div
-		className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-		role="status">
-		<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-			Loading...
-		</span>
-	</div>
-);
 
 export default SignUpForm;
